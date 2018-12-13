c09e0b601834b72037bc94bf6db7015e2fc66c81
alaeeddine123 <alaeeddine.charrik@gmail.com>
2018-12-13 12:04:07 +0100

    route for module zendsite configured but index doesnt show

--- module/zendsite/config/module.config.php
+++ module/zendsite/config/module.config.php
@@ -1,2 +1,24 @@
 <?php
-return array();
+return array(
+'controllers' => array(
+  'invokables' => array('zendsite\Controller\zendsite' => 'zendsite\Controller\zendsiteController'),
+),
+'router'  => array(
+  'routers' => array(
+    'zendsite' => array(
+      'type' => 'segment',
+      'options' => array(
+        'route' => '/zendsite[/:action][/:id]',
+        'constraints' => array (
+          'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+          'id' => '[0-9]+'),
+          'defaults' => array(
+            'controller' => 'zendsite\Controller\zendsite',
+            'action' =>'index'),
+        )
+      )
+    )
+  ),
+  'view_manager' => array('template_path_stack' => array('zendsite' => __DIR__.'/../view'),),
+
+);
