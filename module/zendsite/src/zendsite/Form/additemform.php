01049fae517aba91406af6400eb79bd550cbe875
alaeeddine123 <alaeeddine.charrik@gmail.com>
2018-12-18 14:26:43 +0100

    add item form works

--- module/zendsite/src/zendsite/Form/additemform.php
+++ module/zendsite/src/zendsite/Form/additemform.php
@@ -17,9 +17,7 @@ public function __construct()
        $this->add(array(
             'name' => 'item_name',
             'type' => 'text',
-            'options' => array('label' => 'item name'   )
-           )
-         );
+            'options' => array('label' => 'item name'   ) ) );
 
          $this->add(array(
 
@@ -28,24 +26,26 @@ public function __construct()
                'options' => array('label' => 'item price'   )
               )
             );
+            $this->add(array(
 
-         $this->add(array(
-              'name' => 'item_price',
-              'type' => 'text',
-              'attributes' => array(
-                'value' => 'add item',
-                'id' => 'submitbutton')
-              )
-            );
+                 'name' => 'item_expireddate',
+                 'type' => 'text',
+                  'options' => array('label' => 'item expired date'   )
+                 )
+               );
+
+
+                     $this->add(array(
+
+                 'name' => 'add_item',
+                 'type' => 'submit',
+
+            'attributes' => array(
+              'value' => 'add item',
+              'id' => 'submitbutton') ));
 
 
   }
 }
-
-
-
-
-
-
 
  ?>
